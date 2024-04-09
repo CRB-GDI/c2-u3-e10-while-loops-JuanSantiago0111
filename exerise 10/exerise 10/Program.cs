19bf98cb832c6a372f354156d9f7f30674bc22ba
JuanSantiago0111 <145376152+JuanSantiago0111@users.noreply.github.com>
2024-04-09 11:29:39 -0400

    Changed!

---
+++ exerise 10/exerise 10/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace exerise_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load  Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("***********");
+            string response = Console.ReadLine();
+            if (response == "1")
+            {
+                Console.WriteLine("Create new game");
+            }
+            else if (response == "2")
+            {
+                Console.WriteLine("Loading game");
+            }
+            else if (response == "3")
+            {
+                Console.WriteLine("setting option");
+            }
+            else if (response == "4")
+            {
+                Console.WriteLine("Back to Main menu");
+            }
+            while (response != "4")
+            {
+                Console.WriteLine("***********");
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1 - New Game");
+                Console.WriteLine("2 - Load  Game");
+                Console.WriteLine("3 - Options");
+                Console.WriteLine("4 - Quit");
+                Console.WriteLine("***********");
+                response = Console.ReadLine();
+                if (response == "1")
+                {
+                    Console.WriteLine("Create new game");
+                }
+                else if (response == "2")
+                {
+                    Console.WriteLine("Loading game");
+                }
+                else if (response == "3")
+                {
+                    Console.WriteLine("setting option");
+                }
+                else if (response == "4")
+                {
+                    Console.WriteLine("Back to Main menu");
+                }
+            }
+        }
+    }
+}
